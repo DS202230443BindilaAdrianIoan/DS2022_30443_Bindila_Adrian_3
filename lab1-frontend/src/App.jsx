03397709dc8151cac3eb5f6903ab55c28f88d50c
Adrian <bindila.ioan@gmail.com>
2023-01-06 20:52:19 +0200

    WS?

--- lab1-frontend/src/App.jsx
+++ lab1-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import AdminPage from "./components/AdminPage";
 import ChartPage from "./components/ChartPage";
+import ChatRoom from "./components/ChatRoom";
 import ClientPage from "./components/ClientPage";
 import HomePage from "./components/HomePage";
 import Login from "./components/Login";
@@ -30,6 +31,7 @@ function App() {
             <AdminPage />
           }
         />
+        <Route path="/lol" element={<ChatRoom/>}/>
       </Routes>
     </BrowserRouter>
   );
--- src/main/java/com/ds/dslab1/config/WebSocketConfig.java
+++ src/main/java/com/ds/dslab1/config/WebSocketConfig.java
@@ -1,25 +1,23 @@
 package com.ds.dslab1.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.stereotype.Component;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Component
+@Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
-
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("http://localhost:3000").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker( "/user");
         registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/chatroom","/user");
         registry.setUserDestinationPrefix("/user");
-        //localhost:8080/ws/ + /user/{username}/notify
     }
 }
